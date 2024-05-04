471673954eebb3adbf26d6e3a7ebcddbaafcc26b
Tolunay-c <tolunaycagl@hotmail.com>
2024-05-04 22:06:55 +0300

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-function Title({title,more=false}) {
+function Title({ title, more = false }) {
   return (
     <header className='flex items-center justify-between mb-4'>
-    <NavLink to={more ?? '#'}>
-       <h3 className='text-2xl text-white font-semibold tracking-tight hover:underline '>{title}</h3>
-    </NavLink>
-     {more && (
-         <NavLink className={"text-xs font-semibold uppercase text-link hover:underline tracking-wider"} to={more}>SEE ALL</NavLink>
-     )}
-   </header>
-  )
+      <NavLink to={more || '#'}>
+        <h3 className='text-2xl text-white font-semibold tracking-tight hover:underline'>
+          {title}
+        </h3>
+      </NavLink>
+      {more && (
+        <NavLink to={more} className="text-xs font-semibold uppercase text-link hover:underline tracking-wider">
+          SEE ALL
+        </NavLink>
+      )}
+    </header>
+  );
 }
 
-export default Title
+export default Title;
